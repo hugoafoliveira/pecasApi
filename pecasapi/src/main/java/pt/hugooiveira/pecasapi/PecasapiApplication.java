e124a1191580d5f7f98124b47791b0203bc34127
Hugo Oliveira <hugo15106@gmail.com>
2023-03-25 21:48:30 +0000

    teste

---
+++ pecasapi/src/main/java/pt/hugooiveira/pecasapi/PecasapiApplication.java
@@ -0,0 +1,13 @@
+package pt.hugooiveira.pecasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PecasapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PecasapiApplication.class, args);
+	}
+
+}
---
+++ pecasapi/src/test/java/pt/hugooiveira/pecasapi/PecasapiApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.hugooiveira.pecasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PecasapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
